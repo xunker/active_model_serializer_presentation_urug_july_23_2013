8017d7300c06caebf48c219924204c30ba64bc5f
Matthew Nielsen <xunker@pyxidis.org>
2013-07-23 12:49:25 -0600

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ActiveModelSerializerPresentationUrugJuly232013::Application.config.secret_token = '317053dce775f9b17e50a7839d32646f8ada161c96ca81cb8078f98ae482cf90d59cb3fb8e821718bd4fe9bbc66ae67c9359809287f1e5d5cc9c39fbb419f651'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ActiveModelSerializerPresentationUrugJuly232013::Application.config.session_store :cookie_store, key: '_active_model_serializer_presentation_urug_july_23_2013_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ActiveModelSerializerPresentationUrugJuly232013::Application.config.session_store :active_record_store
